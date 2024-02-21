bf7c93bbff4dd90aa007a1dda85c78231f60f3b1
AntonHran <for.program.learning@gmail.com>
2024-02-22 00:51:36 +0200

    updated with new examples

--- orm_session_new_api.py
+++ orm_session_new_api.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, ForeignKey, create_engine, select
+from sqlalchemy import Integer, String, ForeignKey, create_engine, select, and_, or_, not_, func
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base, Mapped, mapped_column
 
 
@@ -36,11 +36,23 @@ if __name__ == '__main__':
     session.add(n_user)
     n_address = Address(email="smith@email.com", user=n_user)
     session.add(n_address)
+    n_address = Address(email="james_dam@email.com", user=n_user)
+    session.add(n_address)
+    n_address = Address(email="james@email.com", user=n_user)
+    session.add(n_address)
     session.commit()
 
     n_user = User(fullname="Jack Keating")
     session.add(n_user)
     n_address = Address(email="keating@email.com", user=n_user)
+    session.add(n_address)
+    session.commit()
+
+    n_user = User(fullname="Adam Smith")
+    session.add(n_user)
+    n_address = Address(email="adam@email.com", user=n_user)
+    session.add(n_address)
+    n_address = Address(email="adam_smith@email.com", user=n_user)
     session.add(n_address)
     session.commit()
 
@@ -70,8 +82,27 @@ if __name__ == '__main__':
     for row in result:
         print(row.fullname)
 
-    statement = select(User).where(User.fullname.like("Jack%")).where(User.id == 3)
+    statement = select(User).where(User.fullname.like("Jack%")).where(User.id == 3)  # AND
     result = session.execute(statement).scalars()
     for row in result:
         print(row.fullname)
+
+    statement = select(User).where(and_(User.fullname.like("Jack%"), User.id == 3))  # AND
+    result = session.execute(statement).scalars()
+    for row in result:
+        print(row.fullname)
+
+    statement = select(User).where(or_(User.fullname.like("Jack%"), User.id == 3))  # OR
+    result = session.execute(statement).scalars()
+    for row in result:
+        print(row.fullname)
+
+    statement = select(User).order_by(User.fullname)
+    columns = ["id", "fullname"]
+    result = [dict(zip(columns, (row.User.id, row.User.fullname))) for row in session.execute(statement)]
+    print(result)
+
+    statement = select(User.fullname, func.count(Address.id)).join(Address).group_by(User.fullname)
+    res = session.execute(statement).all()
+    print(res)
 
