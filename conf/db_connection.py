125823c35b863cfe7c078a0b0653ab6ba869c956
AntonHran <for.program.learning@gmail.com>
2024-02-24 23:47:52 +0200

    new file

--- conf/db_connection.py
+++ conf/db_connection.py
@@ -18,6 +18,6 @@ port = config.get("DEV_DB", "PORT")
 
 URI = f"postgresql://{user}:{password}@{domain}:{port}/{db_name}"
 
-engine = create_engine(URI, echo=False, pool_size=5, max_overflow=0)
+engine = create_engine(URI, echo=True, pool_size=5, max_overflow=0)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
