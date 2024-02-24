c5af863b273ae259bb11a6cbb15726fa622744b6
AntonHran <for.program.learning@gmail.com>
2024-02-24 22:58:53 +0200

    insert data to db

---
+++ seeds/init.py
@@ -0,0 +1,58 @@
+import random
+
+from faker import Faker
+from sqlalchemy.exc import SQLAlchemyError
+
+from conf.models import Teacher, Student, TeacherStudent
+from conf.db_connection import session
+
+
+STUDENTS_NUMBER = 20
+TEACHERS_NUMBER = 6
+
+fake = Faker("uk-UA")
+
+
+def insert_students():
+    for _ in range(STUDENTS_NUMBER):
+        student = Student(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            phone=fake.phone_number(),
+            address=fake.address()
+        )
+        session.add(student)
+
+
+def insert_teachers():
+    for _ in range(TEACHERS_NUMBER):
+        teacher = Teacher(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            phone=fake.phone_number(),
+            address=fake.address(),
+            start_work=fake.date_between(start_date="-5y")
+        )
+        session.add(teacher)
+
+
+def insert_rel_m2m():
+    students = session.query(Student).all()
+    teachers = session.query(Teacher).all()
+    for student in students:
+        rel = TeacherStudent(teacher_id=random.choice(teachers), student_id=student)
+        session.add(rel)
+
+
+def insert_rel_m2m_():
+    for i in range(STUDENTS_NUMBER):
+        teacher_student = TeacherStudent(
+            teacher_id=random.randint(1, TEACHERS_NUMBER),
+            student_id=i
+        )
+        session.add(teacher_student)
+
+
+def main():
