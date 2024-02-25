810b2cd7d27562a04a0f3d69bba24704cd8d7fd9
AntonHran <for.program.learning@gmail.com>
2024-02-25 14:36:59 +0200

    edited

--- queries.py
+++ queries.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy import and_
 from sqlalchemy.orm import joinedload, subqueryload, outerjoin
 
 from conf.db_connection import session
@@ -30,8 +33,19 @@ def get_teachers():
         print(result)
 
 
+def get_teachers_by_date():
+    teachers = session.query(Teacher).options(joinedload(Teacher.students, innerjoin=True))\
+        .filter(and_(Teacher.start_work >= (datetime(year=2020, month=1, day=1)),
+                     Teacher.start_work <= (datetime(year=2021, month=12, day=31)))).all()
+    for t in teachers:
+        columns = ["id", "fullname", "start_work"]
+        result = [dict(zip(columns, (t.id, t.fullname, t.start_work)))]
+        print(result)
+
+
 if __name__ == "__main__":
     # get_student_join()
-    get_students()
-    print()
-    get_teachers()
+    # get_students()
+    # print()
+    # get_teachers()
+    get_teachers_by_date()
