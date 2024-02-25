3379f29a32dca8d4fb0e4e06927cc8d19024cbf4
AntonHran <for.program.learning@gmail.com>
2024-02-26 00:11:24 +0200

    new consts were added

--- ht/seeds/initial_const.py
+++ ht/seeds/initial_const.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
+from faker import Faker
 
-GROUPS_NUMBER = 3
-STUDENTS_NUMBER = 50
-TEACHERS_NUMBER = 6
-SUBJECTS_NUMBER = 8
-MIN_SUBJECTS_NUMBER = 2
-MAX_SUBJECTS_NUMBER = 5
-START_STUDY = datetime(year=2023, month=9, day=1)
-FINISH_STUDY = datetime(year=2023, month=12, day=24)
-WEEKEND = (5, 6)
+
+GROUPS_NUMBER: int = 3
+STUDENTS_NUMBER: int = 50
+TEACHERS_NUMBER: int = 6
+SUBJECTS_NUMBER: int = 8
+MIN_SUBJECTS_NUMBER: int = 2
+MAX_SUBJECTS_NUMBER: int = 5
+MIN_GRADE: int = 0
+MAX_GRADE: int = 100
+MIN_NUMBER_STUDENTS: int = 4
+MAX_NUMBER_STUDENTS: int = 10
+START_STUDY: datetime = datetime(year=2023, month=9, day=1)
+FINISH_STUDY: datetime = datetime(year=2023, month=12, day=24)
+WEEKEND: tuple = (5, 6)
+
+fake = Faker("uk-UA")
