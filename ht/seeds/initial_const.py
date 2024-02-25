2a11fe721f1eb89729e25c9cfea3437f8d7f61e1
AntonHran <for.program.learning@gmail.com>
2024-02-25 22:21:35 +0200

    edited

---
+++ ht/seeds/initial_const.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+GROUPS_NUMBER = 3
+STUDENTS_NUMBER = 50
+TEACHERS_NUMBER = 6
+SUBJECTS_NUMBER = 8
+MIN_SUBJECTS_NUMBER = 2
+MAX_SUBJECTS_NUMBER = 5
+START_STUDY = datetime(year=2023, month=9, day=1)
+FINISH_STUDY = datetime(year=2023, month=12, day=24)
+WEEKEND = (5, 6)
