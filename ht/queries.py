351ee04693429b95dd402207529f35fd2368340c
AntonHran <for.program.learning@gmail.com>
2024-02-27 00:14:11 +0200

    select 2 was added

--- ht/queries.py
+++ ht/queries.py
@@ -10,5 +10,11 @@ def select_01():
     return result
 
 
+def select_02():
+    result = session.query(Student.id, Student.full_name, func.round(func.avg(Grade.grade), 2).label("average_grade"))\
+        .select_from(Grade).join(Student).filter(Grade.subject_id == 1).group_by(Student.id).order_by(desc("average_grade")).limit(1).all()
+    return result
 
+
+# print(select_01())
 print(select_02())
