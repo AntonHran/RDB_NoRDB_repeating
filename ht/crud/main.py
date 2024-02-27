c0f3d36259894df2f91b95c7d5adef704ea7d5f7
AntonHran <for.program.learning@gmail.com>
2024-02-28 00:02:48 +0200

    functions for model were added

--- ht/crud/main.py
+++ ht/crud/main.py
@@ -3,7 +3,7 @@ import argparse
 from create import *
 from read import *
 from update import *
-from delete import *
+from remove import *
 
 
 parser = argparse.ArgumentParser(description='CLI DB CRUD app')
@@ -11,21 +11,20 @@ parser.add_argument('--action', '-a', help='Commands: create, read, update, dele
 parser.add_argument('--model', '-m', help='use before the certain table for some changes')
 # arguments of tables
 parser.add_argument('--id')
-parser.add_argument('--grname', help='name of a group')
-parser.add_argument('--fname', help='first name')
-parser.add_argument('--lname', help='last name')
-# parser.add_argument('--fullname', help='full name')
+parser.add_argument('--group_name', help='name of a group')
+parser.add_argument('--firstname', help='first name')
+parser.add_argument('--lastname', help='last name')
 parser.add_argument('--email')
 parser.add_argument('--phone')
 parser.add_argument('--address')
 parser.add_argument('--stw', help='Date of work staring')
 parser.add_argument('--subject')
 parser.add_argument('--grade')
-parser.add_argument('--stid')
-parser.add_argument('--groupid')
-parser.add_argument('--lectorid')
-parser.add_argument('--subid')
-parser.add_argument('--lesdate')
+parser.add_argument('--stud_id')
+parser.add_argument('--group_id')
+parser.add_argument('--lector_id')
+parser.add_argument('--sub_id')
+parser.add_argument('--lesson_date')
 
 arguments = parser.parse_args()
 my_arg = vars(arguments)
@@ -34,21 +33,21 @@ my_arg = vars(arguments)
 action = my_arg.get('action')
 model = my_arg.get('model')
 id_ = my_arg.get('id')
-group_name = my_arg.get('grname')
-first_name = my_arg.get('fname')
-last_name = my_arg.get('lname')
+group_name = my_arg.get('group_name')
+first_name = my_arg.get('firstname')
+last_name = my_arg.get('lastname')
 full_name = my_arg.get('fullname')
 email = my_arg.get('email')
 phone = my_arg.get('phone')
 address = my_arg.get('address')
-start_work = my_arg.get('stw')
+start_work = my_arg.get('start_work')
 subject_name = my_arg.get('subject')
 grade = my_arg.get('grade')
-group_id = my_arg.get('groupid')
-student_id = my_arg.get('stid')
-lector_id = my_arg.get('lectorid')
-subject_id = my_arg.get('subid')
-lesson_date = my_arg.get('lesdate')
+group_id = my_arg.get('group_id')
+student_id = my_arg.get('stud_id')
+lector_id = my_arg.get('lector_id')
+subject_id = my_arg.get('sub_id')
+lesson_date = my_arg.get('lesson_date')
 
 
 def main():
@@ -57,13 +56,13 @@ def main():
             if model == 'Group':
                 create_group(group_name)
             elif model == 'Student':
-                create_student(first_name, last_name, email, phone, address, group_id)
+                create_student(first_name, last_name, int(group_id))
             elif model == 'Lector':
-                create_lector(first_name, last_name, email, phone, address, start_work)
+                create_lector(first_name, last_name, start_work)
             elif model == 'Subject':
-                create_subject(subject_name, lector_id)
-            elif model == 'Mark':
-                create_mark(grade, lesson_date, student_id, subject_id)
+                create_subject(subject_name)
+            elif model == 'Grade':
+                create_grade(grade, lesson_date, student_id, subject_id)
         case 'read':
             res = read_table(model, id_)
             return_read_res(res, model)
