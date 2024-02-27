3fcaf7b158acb48a46422eb7ba7073a4287c2f9b
AntonHran <for.program.learning@gmail.com>
2024-02-28 00:48:13 +0200

    billet

---
+++ ht/crud/update.py
@@ -0,0 +1,8 @@
+from sqlalchemy.exc import SQLAlchemyError
+
+from ht.config.db_connect import session
+from ht.config.models_ import Group, Student, Subject, Teacher, Grade
+
+
+def update_table(data):
+    ...
