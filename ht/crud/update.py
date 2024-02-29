d9ad449e6e15097893e4886e07ec5b049b1418ef
AntonHran <for.program.learning@gmail.com>
2024-02-29 23:13:33 +0200

    edited

--- ht/crud/update.py
+++ ht/crud/update.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -8,7 +9,21 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from config.db_connect import session
 from config.models_ import Group, Student, Subject, Teacher, Grade
+from read import return_model
 
 
-def update_table(data):
-    ...
+def update_table(data: dict):
+    data = get_data(data)
+    print(data)
+
+
+def get_data(arguments: dict):
+    return {key: value for key, value in arguments.items() if value}
+
+
+def search_field(data: dict):
+    table_model = return_model(data.get("model"))
+    record = session.query(table_model).filter_by(id=data.get("id"))
+    for el in data.keys():
+        if getattr(table_model, el):
+            record.update()
