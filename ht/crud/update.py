c65cb2678a257d09d421a510d6d8fc644e6ac493
AntonHran <for.program.learning@gmail.com>
2024-02-29 23:31:35 +0200

    edited

--- ht/crud/update.py
+++ ht/crud/update.py
@@ -8,13 +8,13 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from config.db_connect import session
-from config.models_ import Group, Student, Subject, Teacher, Grade
+from config.models_ import Group, Student, Subject, Teacher, Grade, Base
 from read import return_model
 
 
 def update_table(data: dict):
     data = get_data(data)
-    print(data)
+    search_field(data)
 
 
 def get_data(arguments: dict):
@@ -25,5 +25,14 @@ def search_field(data: dict):
     table_model = return_model(data.get("model"))
     record = session.query(table_model).filter_by(id=data.get("id"))
     for el in data.keys():
-        if getattr(table_model, el):
-            record.update()
+        field = search_atr(table_model, el)
+        if field:
+            record.update({getattr(record, field): data.get(el)})
+    session.commit()
+    session.close()
+
+
+def search_atr(table: Base, key: str) -> str:
+    for atr in vars(table).keys():
+        if re.match(atr, key):
+            return atr
