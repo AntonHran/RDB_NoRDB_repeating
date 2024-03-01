e07beb4fb513540a18bf0d42f18bd648655e1972
AntonHran <for.program.learning@gmail.com>
2024-03-01 23:06:02 +0200

    new approach

--- ht/crud/create.py
+++ ht/crud/create.py
@@ -8,8 +8,9 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from config.db_connect import session
-from config.models_ import Group, Student, Subject, Teacher, Grade
+from config.models_ import Group, Student, Subject, Teacher, Grade, Base
 from read import return_model
+from update import get_data, search_atr
 
 
 def create_group(gr_name: str):
@@ -68,18 +69,6 @@ def create_lector(first_name: str, last_name: str, start_date: str):
         session.close()
 
 
-def parse_data(str_date: str) -> datetime.date:
-    date_ = ""  # = datetime.date.today()
-    try:
-        year, month, day = str_date.split("-")
-        date_ = datetime.date(year=year, month=month, day=day)
-        return date_
-    except TypeError as err:
-        print(err)
-    finally:
-        return date_
-
-
 def create_grade(st_id: int, sub_id: int, grade: int, date: str):
     if check_stud_id(st_id) and check_subject_id(sub_id):
         try:
@@ -108,12 +97,8 @@ def check_subject_id(sub_id: int) -> bool:
     return True if subject else False
 
 
-def create_record(table_name, data: dict):
-    table = return_model(table_name)
+def create_record(new_record):
     try:
-        new_record = table()
-        for key in data.keys():
-            setattr(new_record, key, data.get(key))
         session.add(new_record)
         session.commit()
     except SQLAlchemyError as err:
@@ -121,3 +106,49 @@ def create_record(table_name, data: dict):
         print(err)
     finally:
         session.close()
+
+
+def fill_data(data: dict, table: Base, new_rec: Base) -> Base:
+    for key in data.keys():
+        field_to_fill = search_atr(table, key)
+        if field_to_fill:
+            try:
+                val = analyze_attrs(table, key, data.get(key))
+                setattr(new_rec, field_to_fill, val)
+            except (TypeError, ) as err:
+                raise err
+    return new_rec
+
+
+def analyze_attrs(table: Base, key: str, val: str | int):
+    if key.endswith("_id"):
+        if search(table, val):
+            return val
+    if key.endswith("_date"):
+        return parse_data(val)
+    return val
+
+
+def search(table: Base, val: int):
+    subject = session.query(table).filter_by(id=val).first()
+    return True if subject else False
+
+
+def parse_data(str_date: str) -> datetime.date:
+    date_ = ""  # = datetime.date.today()
+    try:
+        year, month, day = str_date.split("-")
+        date_ = datetime.date(year=year, month=month, day=day)
+        return date_
+    except TypeError as err:
+        print(err)
+    finally:
+        return date_
+
+
+def create_main(data: dict):
+    data = get_data(data)
+    table = return_model(data.get("model"))
+    new_record = table()
+    filled_rec = fill_data(data, table, new_record)
+    create_record(filled_rec)
