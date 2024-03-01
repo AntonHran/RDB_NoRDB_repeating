d5e55130cc4f3291b067862f7b341b4f56c68183
AntonHran <for.program.learning@gmail.com>
2024-03-01 23:05:52 +0200

    changes for checking a new approach

--- ht/crud/main.py
+++ ht/crud/main.py
@@ -1,6 +1,6 @@
 import argparse
 
-from create import *
+from create import create_main
 from read import read_result
 from update import update_table
 from remove import remove_row
@@ -17,7 +17,7 @@ parser.add_argument('--lastname', help='last name')
 parser.add_argument('--email')
 parser.add_argument('--phone')
 parser.add_argument('--address')
-parser.add_argument('--start_w', help='Date of work starting')
+parser.add_argument('--start_date', help='Date of work starting')
 parser.add_argument('--subject')
 parser.add_argument('--grade')
 parser.add_argument('--stud_id')
@@ -33,38 +33,29 @@ my_arg = vars(arguments)
 action = my_arg.get('action')
 model = my_arg.get('model')
 id_ = my_arg.get('id')
-group_name = my_arg.get('group_name')
+"""group_name = my_arg.get('group_name')
 first_name = my_arg.get('firstname')
 last_name = my_arg.get('lastname')
 full_name = my_arg.get('fullname')
 email = my_arg.get('email')
 phone = my_arg.get('phone')
 address = my_arg.get('address')
-start_work = my_arg.get('start_work')
+start_work = my_arg.get('start_work_date')
 subject_name = my_arg.get('subject')
 grade = my_arg.get('grade')
 group_id = my_arg.get('group_id')
 student_id = my_arg.get('stud_id')
 lector_id = my_arg.get('lector_id')
 subject_id = my_arg.get('subj_id')
-lesson_date = my_arg.get('lesson_date')
+lesson_date = my_arg.get('lesson_date')"""
 
-args = (model, id_, group_name, first_name, last_name, full_name, email, phone, )
+# args = (model, id_, group_name, first_name, last_name, full_name, email, phone, )
 
 
 def main():
     match action:
         case 'create':
-            if model == 'Group':
-                create_record("Group", {"name": group_name})
-            elif model == 'Student':
-                create_student(first_name, last_name, int(group_id))
-            elif model == 'Lector':
-                create_lector(first_name, last_name, start_work)
-            elif model == 'Subject':
-                create_subject(subject_name)
-            elif model == 'Grade':
-                create_grade(grade, lesson_date, student_id, subject_id)
+            create_main(my_arg)
         case 'read':
             read_result(model, int(id_))
         case 'update':
@@ -74,7 +65,7 @@ def main():
             if result > 0:
                 print(f'Removed: {result}')
             else:
-                print('Not found such task')
+                print('Such a record was not found')
         case _:
             print('Nothing...')
 
